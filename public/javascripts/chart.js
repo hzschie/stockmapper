eddabb47b7107ac408ff5b023b284048cbf218f2
Amit Schechter <amit@meetamit.com>
2012-05-22 17:47:01 -0400

    Indicator for most active stock.

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,8 +1,14 @@
 (function() {
   function Chart($container) {
-    var svg = d3.select($container[0]).append('svg'),
-        models = null;
-    
+    var models = null,
+        svg = d3.select($container[0]).append('svg'),
+        mostActive = svg.append('g').attr('class', 'most_active').attr('fill', '#76f9fb'),
+        $mostActive = $container.find('.most_active');
+        
+    mostActive.append('text').text('VOLUME').attr('dy', 4);
+    mostActive.append('text').attr('class', 'sym').attr('dy', 17);
+    mostActive.append('path').attr('stroke', '#76f9fb').attr('stroke-width', 2);
+
     this.setModels = function(_models) {
       if(models) {
         models.off('reset', redraw);
@@ -95,53 +101,80 @@
       changeTicksText.exit()
         .remove();
         
-        // Render Volume ticks
-        var tickValues = volumeY.ticks(4).slice(1),
-            tickValues = d3.scale.sqrt()
-              .domain([0, tickValues[0]])
-              .range([0, volumeY(tickValues[0])]).ticks(4).slice(1).concat(tickValues),
-            volumeTickY = function(val) { return paddingTop + chgMaxH + Math.round(volumeY(val)) + .5; },
-            changeTicks = svg.selectAll('line.vol').data(tickValues),
-            changeTicksText = svg.selectAll('text.vol').data(tickValues);
-        changeTicks.enter().append('line')
-          .attr('class', 'vol')
-          .attr('stroke-dasharray', '2 2');
-        changeTicks
-          .attr('stroke', '#555555')
-          .attr('x1', 0)
-          .attr('x2', w)
-          .attr('y1', volumeTickY)
-          .attr('y2', volumeTickY);
-        changeTicks.exit()
-          .remove();
+      // Render Volume ticks
+      var tickValues = volumeY.ticks(4).slice(1),
+          tickValues = d3.scale.sqrt()
+            .domain([0, tickValues[0]])
+            .range([0, volumeY(tickValues[0])]).ticks(4).slice(1).concat(tickValues),
+          volumeTickY = function(val) { return paddingTop + chgMaxH + Math.round(volumeY(val)) + .5; },
+          changeTicks = svg.selectAll('line.vol').data(tickValues),
+          changeTicksText = svg.selectAll('text.vol').data(tickValues);
+      changeTicks.enter().append('line')
+        .attr('class', 'vol')
+        .attr('stroke-dasharray', '2 2');
+      changeTicks
+        .attr('stroke', '#555555')
+        .attr('x1', 0)
+        .attr('x2', w)
+        .attr('y1', volumeTickY)
+        .attr('y2', volumeTickY);
+      changeTicks.exit()
+        .remove();
 
-        // Render Volume ticks' text
-        changeTicksText.enter().append('text')
-          .attr('class', 'vol')
-          .attr('text-anchor', 'end')
-          .attr('fill', '#bbb')
-          .attr('dy', -2)
-          .attr('x', w)
-          .attr('y', volumeTickY);
-        changeTicksText
-          .attr('x', w)
-          .attr('y', volumeTickY)
-          .text(function(val) {
-            if(val >= 1e+12)
-              return val / 1e+12 + 'T';
-            else if(val >= 1e+9)
-              return val / 1e+9 + 'B';
-            else if(val >= 1e+6)
-              return val / 1e+6 + 'M';
-            else if(val >= 1e+3)
-              return val / 1e+3 + 'K';
-            else
-              return String(val);
-          });
-        changeTicksText.exit()
-          .remove();
-          
-        console.log(Date.now() - tt + ' ms, CHART redraw');
+      // Render Volume ticks' text
+      changeTicksText.enter().append('text')
+        .attr('class', 'vol')
+        .attr('text-anchor', 'end')
+        .attr('fill', '#bbb')
+        .attr('dy', -2)
+        .attr('x', w)
+        .attr('y', volumeTickY);
+      changeTicksText
+        .attr('x', w)
+        .attr('y', volumeTickY)
+        .text(function(val) {
+          if(val >= 1e+12)
+            return val / 1e+12 + 'T';
+          else if(val >= 1e+9)
+            return val / 1e+9 + 'B';
+          else if(val >= 1e+6)
+            return val / 1e+6 + 'M';
+          else if(val >= 1e+3)
+            return val / 1e+3 + 'K';
+          else
+            return String(val);
+        });
+      changeTicksText.exit()
+        .remove();
+  
+      var mostActiveStock = models.where({ 'volume': d3.max(models.models, Stock.volume) })[0];
+      if(mostActiveStock) {
+        var arrowIndex = _.indexOf(models.models, mostActiveStock),
+            mult = arrowIndex > models.models.length / 2 ? -1 : 1,
+            arrowX = barX(null, arrowIndex + 1*mult),
+            arrowY = volumeTickY(mostActiveStock.get('volume')) - 44;
+        $mostActive.show().find('path').attr('d', [
+          'M' + arrowX + ',' + arrowY,
+          'l' + 20*mult + ',0',
+          'M' + arrowX + ',' + arrowY,
+          'l' + 7*mult + ',-5',
+          'M' + arrowX + ',' + arrowY,
+          'l' + 7*mult + ',5'
+        ].join(' '));
+        
+        $mostActive.find('text').attr({ 
+          x: arrowX,
+          y: arrowY,
+          dx: 25 * mult,
+          'text-anchor': mult == 1 ? 'start' : 'end'
+        });
+        $mostActive.find('text.sym').text('Most Active Stock: ' + mostActiveStock.get('sym'));
+      }
+      else {
+        $mostActive.hide();
+      }
+
+      console.log(Date.now() - tt + ' ms, CHART redraw');
     };
   }
   mapper.Chart = Chart;
