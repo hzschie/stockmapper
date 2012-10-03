0b55646ad9d4a3a7e9c3f4b2b434ecc9b1832bdf
Amit Schechter <amit@meetamit.com>
2012-10-03 19:26:04 -0400

    Ability to turn off map highlighting.

--- public/javascripts/map_ui.js
+++ public/javascripts/map_ui.js
@@ -1,6 +1,102 @@
 (function() {
   mapper.MapHighlights = MapHighlights;
   
+  function MapHighlights($container) {
+    var $performance = $('.panel .performance', $container);
+    
+    // DEFINITIONS OF HIGHLIGHTERS
+    mapper.highlighters = {
+      active: new ActivityHighlighter(null, $('.panel .active', $container)),
+      best: new PerformanceHighlighter(
+        {
+          type: "Best",
+          val: .95,
+          period: 'p52wk'
+        },
+        $('<div class="best performance content">' + $performance.html() + '</div>').appendTo($performance.parent()),
+        $('#best')
+      ),
+      worst: new PerformanceHighlighter(
+        {
+          type: "Worst",
+          val: .95,
+          period: 'p52wk'
+        },
+        $performance.addClass('worst'),
+        $('#worst')
+      )
+    };
+    
+    // The "NONE" highlighter
+    mapper.highlighters[mapper.SelectorButtons.NONE] = {
+      fn: function() { return false; },
+      ready: function(callback) { callback(); },
+      on: function() {},// no-op
+      off: function() {}// no-op
+    };
+    
+    var currentType = 'active',
+        selector = new mapper.SelectorButtons($container, setHighlightType, true),
+        $panel = $('.panel', $container).fadeOut(),
+        $plus = $('.plus', $container),
+        
+        _this = this;
+        
+    setHighlightType(currentType);
+        
+    $plus.click(function() {
+      if($panel.hasClass('disabled')) _this.showPanel();
+      else _this.hidePanel();
+    });
+    
+    this.showPanel = function() {
+      $panel.removeClass('disabled');
+      $panel.fadeIn(150);
+      $plus.addClass('minus');
+    };
+    
+    this.hidePanel = function() {
+      $panel.fadeOut(150, function() { $panel.addClass('disabled'); });
+      $plus.removeClass('minus');
+    };
+    
+    function setHighlightType(id) {
+      if(currentType) mapper.highlighters[currentType].off('change');
+      
+      currentType = id;
+      selector.setCurrent(currentType);
+      
+      mapper.highlighters[currentType].on('change', updateModels);
+
+      $panel.children().removeClass('current');
+      $panel.find('.' + currentType).addClass('current');
+      
+      updateModels();
+    }
+    
+    function updateModels() {
+      var highlighter = mapper.highlighters[currentType];
+      highlighter.ready(
+        function() {
+          $container.removeClass('pending');
+          var fn = highlighter.fn;
+          mapper.stocks.each(function(s) {
+            s.setHighlightFunc(fn);
+          });
+        },
+        function() {
+          setTimeout(function() {
+            $container.addClass('pending');
+          },0);
+          var fn = function() { return false; };
+          mapper.stocks.each(function(s) {
+            s.setHighlightFunc(fn);
+          });
+        }
+      );
+    }
+  }
+  
   function ActivityHighlighter(opts, $ui) {
     _.extend(this, Backbone.Events);
     var slider = new mapper.SliderAndInput(
@@ -89,93 +185,6 @@
       _this.val = val;
       if(slider.val() != _this.val) slider.val(_this.val);
       if(!silent) _this.trigger('change');
-    }
-  }
-  
-  function MapHighlights($container) {
-    var $performance = $('.panel .performance', $container);
-    mapper.highlighters = {
-      active: new ActivityHighlighter(null, $('.panel .active', $container)),
-      best: new PerformanceHighlighter(
-        {
-          type: "Best",
-          val: .95,
-          period: 'p52wk'
-        },
-        $('<div class="best performance content">' + $performance.html() + '</div>').appendTo($performance.parent()),
-        $('#best')
-      ),
-      worst: new PerformanceHighlighter(
-        {
-          type: "Worst",
-          val: .95,
-          period: 'p52wk'
-        },
-        $performance.addClass('worst'),
-        $('#worst')
-      )
-    };
-    
-    var currentType = 'active',
-        selector = new mapper.SelectorButtons($container, setHighlightType, true),
-        $panel = $('.panel', $container).fadeOut(),
-        $plus = $('.plus', $container),
-        
-        _this = this;
-        
-    setHighlightType(currentType);
-        
-    $plus.click(function() {
-      if($panel.hasClass('disabled')) _this.showPanel();
-      else _this.hidePanel();
-    });
-    
-    this.showPanel = function() {
-      $panel.removeClass('disabled');
-      $panel.fadeIn(150);
-      $plus.addClass('minus');
-    };
-    
-    this.hidePanel = function() {
-      $panel.fadeOut(150, function() { $panel.addClass('disabled'); });
-      $plus.removeClass('minus');
-    };
-    
-    function setHighlightType(id) {
-      if(currentType) mapper.highlighters[currentType].off('change');
-      
-      currentType = id;
-      selector.setCurrent(currentType);
-      mapper.highlighters[currentType].on('change', updateModels);
-      
-      if(currentType == mapper.SelectorButtons.NONE) console.log('none');
-
-      $panel.children().removeClass('current');
-      $panel.find('.' + currentType).addClass('current');
-      
-      updateModels();
-    }
-    
-    function updateModels() {
-      var highlighter = mapper.highlighters[currentType];
-      highlighter.ready(
-        function() {
-          $container.removeClass('pending');
-          var fn = highlighter.fn;
-          mapper.stocks.each(function(s) {
-            s.setHighlightFunc(fn);
-          });
-        },
-        function() {
-          setTimeout(function() {
-            $container.addClass('pending');
-          },0);
-          var fn = function() { return false; };
-          mapper.stocks.each(function(s) {
-            s.setHighlightFunc(fn);
-          });
-        }
-      );
     }
   }
 })();
