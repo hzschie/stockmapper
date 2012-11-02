004ce55501f2b686fd99ff3b67e326a28af7886e
Amit Schechter <amit@meetamit.com>
2012-11-02 13:54:33 -0400

    Yahoo source to query every 5 minutes. Some foreside configing.

---
+++ public/javascripts/domains/foreside.js
@@ -0,0 +1,28 @@
+(function() {
+  /* Mapping of Array positions to attributes that get applied to instances of Stock on data updates */
+  mapper.config.getStockUpdateFields = function() {
+    var numeric = mapper.MapperModel.numeric,
+        parseMCap = mapper.MapperModel.parseMarketCapString;
+    return [
+      null,
+      null, 
+      { name:'lastTrade', formatter:numeric },
+      { name:'timestamp', formatter:numeric },
+      null,
+      { name:'change', formatter:numeric },
+      { name:'previous', formatter:numeric },
+      { name:'open', formatter:numeric },
+      { name:'high', formatter:numeric },
+      { name:'low', formatter:numeric },
+      { name:'volume', formatter:numeric },
+      { name:'changePct', formatter:parseFloat },
+      { name:'marketCapString', formatter:function(val, field, hash) { hash.marketCap = parseMCap(val); return val; } },
+      { name:'avgVolume', formatter:numeric }
+    ];
+  };
+
+  /* Mapping of Array positions to attributes that get applied to instances of StockGroup on data updates */
+  mapper.config.getGroupUpdateFields = function() {
+    return mapper.config.getStockUpdateFields();
+  };
+})();
