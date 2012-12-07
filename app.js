b4e3f1d8a25454ac74d2f5c72d37136cfd348ded
Amit Schechter <amit@meetamit.com>
2012-12-08 02:14:23 +0700

    Extract login support into separate, optional module. iPhone support on login page.

--- app.js
+++ app.js
@@ -7,41 +7,8 @@ var express = require('express'),
     http = require('http'),
     fs = require('fs'),
     
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    
     dataRoutes = require('./routes/data.js');
-
-passport.serializeUser(function(user, done) {
-  // console.log('serialize', user.id);
-  done(null, user.id);
-});
-passport.deserializeUser(function(str, done) {
-  // console.log('DEserialize', str);
-  done(null, { id: str });
-});
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    if(password) {
-      password = password.toLowerCase();
-      if(['foreside', 'cnbc', 'state street', '2n'].indexOf(password) > -1) {
-        console.log('LOG IN: ' + password);
-        done(null, { id: password });
-      }
-      else {
-        console.log('FAILED ATTEMP: ' + password);
-        done(null, false, { message: 'Invalid password' });
-      }
-    }
-    // process.nextTick(function () {
-    //   done(null, { id: password });
-    // });
-    // if (err) { return done(err); }
-    // if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-    // if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-    // return done(null, user);
-  }
-));
+    loginSupport = /true/i.test(process.env.REQUIRE_LOGIN) ? require('./lib/login_support.js') : null;// Gets require()'ed only if needed
 
 // NODE APP CONFIGURATION    
 var app = express(),
@@ -67,11 +34,7 @@ app.configure(function(){
   app.use(express['static'](__dirname + '/public'));
   app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   
-  app.use('/', express.cookieParser());
-  app.use('/', express.bodyParser());
-  app.use('/', express.session({ secret: 'keyboard cat' }));
-  app.use('/', passport.initialize());
-  app.use('/', passport.session());
+  if(loginSupport) loginSupport(app);
   
   app.use(app.router);
 });
@@ -83,8 +46,8 @@ BundleUp(app, __dirname + '/lib/assets', {
   staticRoot: __dirname + '/public/',
   staticUrlRoot:'/',
   bundle: (/true/i.test(process.env.BUNDLE)),
-  minifyCss: false,//true,
-  minifyJs: false//true
+  minifyCss: true,
+  minifyJs: true
 });
 if(/true/i.test(process.env.VERBOSE)) console.log("process.env = ", process.env);
 
@@ -123,45 +86,31 @@ app.get('/scratchpad', function(req, res) {
   });
 });
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
-}
-
-app.get('/login', function(req, res) {
-  res.render('domains/' + dataDomain + '/login', {
-    analyticsCode: process.env.ANALYTICS_CODE
-  });
-});
-
-
-app.post('/login',
-  passport.authenticate('local', { failureRedirect: '/login' }),
+var mainRouteArgs = ['/*'];
+if(loginSupport) mainRouteArgs.push(loginSupport.ensureAuthenticated);
+mainRouteArgs.push(
   function(req, res) {
-    res.redirect('/');
+    if(!groups || !stocks) {
+      // In dynamic mode, if the definitions aren't ready, we wait for them.
+      var handler = arguments.callee;// This response handler
+      definitions.on('update', function() { handler(req, res); });
+      return;
+    }
+  
+    var ua = req.headers['user-agent'],
+        isTablet = /ipad/i.test(ua) || req.query.tablet || false,
+        isMobile = !isTablet && (/mobile/i.test(ua) || req.query.mobile || false);
+      
+    res.render(dataDomain, {
+      dataDomain: dataDomain,
+      dataConfig: dataConfig,
+      groups: groups,
+      stocks: stocks,
+      isTablet: isTablet,
+      isMobile: isMobile,
+      useWebSocket: io != null,
+      analyticsCode: process.env.ANALYTICS_CODE
+    });
   }
 );
-  
-app.get('/*', ensureAuthenticated, function(req, res) {
-  if(!groups || !stocks) {
-    // In dynamic mode, if the definitions aren't ready, we wait for them.
-    var handler = arguments.callee;// This response handler
-    definitions.on('update', function() { handler(req, res); });
-    return;
-  }
-  
-  var ua = req.headers['user-agent'],
-      isTablet = /ipad/i.test(ua) || req.query.tablet || false,
-      isMobile = !isTablet && (/mobile/i.test(ua) || req.query.mobile || false);
-      
-  res.render(dataDomain, {
-    dataDomain: dataDomain,
-    dataConfig: dataConfig,
-    groups: groups,
-    stocks: stocks,
-    isTablet: isTablet,
-    isMobile: isMobile,
-    useWebSocket: io != null,
-    analyticsCode: process.env.ANALYTICS_CODE
-  });
-});
+app.get.apply(app, mainRouteArgs);
