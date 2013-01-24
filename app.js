949a4148636a754db2946a2f3b6c2a843385f8eb
Amit Schechter <amit@meetamit.com>
2013-01-24 13:52:07 -0500

    Add gzip compression. Replace blufin logo imgs.

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
 
+  app.use(express.compress());
   app.use(express['static'](__dirname + '/public'));
   
   var favicon = __dirname + '/public/images/domains/' + dataDomain + '/favicon.ico';
