bbf9d9cfd0676f396433f7f214a8596d2b7ed65b
Chia-Yen Hung <chiayen.hung@nyu.edu>
2013-07-31 18:12:08 -0400

    add change

--- public/javascripts/domains/foreside_website.js
+++ public/javascripts/domains/foreside_website.js
@@ -14,8 +14,6 @@ $(document).ready(function() {
 function checkChange(){
 	var changes = $('li > .change');
 	for(var i = 0; i < changes.length; i++){
-		console.log(typeof $(changes[i]).text());
-		var change = $(changes[i]).text();
 		if(change.startsWith("-"))
 			if(!$(changes[i]).hasClass('down'))
 				$(changes[i]).addClass('down');
@@ -27,6 +25,8 @@ function quote(){
         url: '/quote/^ETFCOMP',
         success: function(data){
           $('.etfcomp_quote > .price').text(data['last_trade_price']);
+          if(data['change'] < 0)
+          	$('.etfcomp_quote > .change').addClass('down');
           $('.etfcomp_quote > .change').text(data['change'] + ' (' + data['change_percent'] + ')');
         },
         error:function(){console.log('error');},
