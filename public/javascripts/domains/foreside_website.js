22424bbe95f4d5a4416d7ce1c4fcb4ffc01c64a0
Chia-Yen Hung <chiayen.hung@nyu.edu>
2013-07-31 17:53:44 -0400

    add check change negative

--- public/javascripts/domains/foreside_website.js
+++ public/javascripts/domains/foreside_website.js
@@ -6,4 +6,30 @@ $(document).ready(function() {
     function() { $productsSubMenu.hide(); }
   );
   
+  quote();
+  checkChange();
+  setInterval(quote, 60000);
 });
+
+function checkChange(){
+	var changes = $('li > .change');
+	for(var i = 0; i < changes.length; i++){
+		console.log(typeof $(changes[i]).text());
+		var change = $(changes[i]).text();
+		if(change.startsWith("-"))
+			if(!$(changes[i]).hasClass('down'))
+				$(changes[i]).addClass('down');
+	}
+}
+
+function quote(){
+      $.ajax({
+        url: '/quote/^ETFCOMP',
+        success: function(data){
+          $('.etfcomp_quote > .price').text(data['last_trade_price']);
+          $('.etfcomp_quote > .change').text(data['change'] + ' (' + data['change_percent'] + ')');
+        },
+        error:function(){console.log('error');},
+        dataType: 'JSON',
+      });
+  }
